fd3d93978f19dd599ff413f01f6d3a5c0746b567
Connor Wilson <monkeytwins2003@gmail.com>
2023-04-28 20:55:03 +0000

    Tests now work, 2.2.3 finished on GE07

--- spec/features/tasks_feature_spec.rb
+++ spec/features/tasks_feature_spec.rb
@@ -1,9 +1,12 @@
 require "rails_helper"
 
 RSpec.feature "Tasks", type: :feature do
+  
   context "Update task" do
     let(:task) { Task.create(chore: "Generic", name: "Generic", time: 2, description: "Generic Description") }
     before(:each) do
+      user = FactoryBot.create(:user)
+      login_as(user)
       visit edit_task_path(task)
     end
 
@@ -15,6 +18,50 @@ RSpec.feature "Tasks", type: :feature do
       expect(page).to have_content("Task was successfully updated")
     end
 
+  end
+
+  context "Login" do
+    scenario "should sign up" do
+      visit root_path
+      click_link 'Sign up'
+      within("form") do
+        fill_in "Email", with: "testing@test.com"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Sign up"
+      end
+      expect(page).to have_content("Welcome! You have signed up successfully.")
+    end
+
+
+
+
+    scenario "should log in" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit root_path
+      expect(page).to have_content("Logged in")
+    end
+  end
+
+
+  context "Create task" do
+    before(:each) do 
+        user = FactoryBot.create(:user)
+        login_as(user)
+        visit new_task_path()
+    end
+
+    scenario "should succeed" do
+      within("form") do
+        fill_in "Chore", with: "Generic Chore"
+        fill_in "Name", with: "Generic Name"
+        fill_in "Time", with: 1;
+        fill_in "Description", with: "Generic Description"
+      end
+      click_button "Create Task"
+      expect(page).to have_content("Task was successfully created")
+    end
   end
 
 end
